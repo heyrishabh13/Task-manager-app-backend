2a7138f73a469fb4ac0dbc5ec408de2a41d342e4
Rishabh Kamboj <heyrishabh13@gmail.com>
2026-06-07 17:05:25 +0530

    cors added

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
+
+app.use(cors());
 
 const mongoose = require("mongoose");
 
